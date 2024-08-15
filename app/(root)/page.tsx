9819a89a1d36229c8ae021f32c246fd25a99e1d3
Duy Quang <ndquang191@gmail.com>
2024-08-15 09:31:18 +0700

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,30 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 export default function Home() {
-	return <main>Hi</main>;
+	const loggedIn = { firstName: "Quang", lastName: "Nguyen Duy" };
+	return (
+		<section className="home">
+			<div className="home-content">
+				<header className="home-header">
+					<HeaderBox
+						type="greeting"
+						title="Welcome"
+						subtext="Access your financial data"
+						user={loggedIn.firstName}
+					/>
+
+					<TotalBalanceBox accounts={[]} totalBanks={0} totalCurrentBalance={10000} />
+				</header>
+			</div>
+
+			{/* righsidebar */}
+			<RightSideBar
+				user={loggedIn}
+				transaction={[]}
+				banks={[{ currentBalance: 1000 }, { currentBalance: 2000 }]}
+			/>
+		</section>
+	);
 }
